3269a18df2010ee277b0761140c397634a0ab80e
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-03-02 03:03:06 -0500

    more styling

--- client/src/pages/Auth/SignUp.jsx
+++ client/src/pages/Auth/SignUp.jsx
@@ -1,33 +1,59 @@
-import React from 'react';
-import './SignUp.css';
+import React from "react";
+import "./SignUp.css";
 
 function SignUp() {
-    return (
-        <form className="signup">
-            <div className="signup__container">
-                <h1>Sign Up</h1>
-            </div>
-            <input
-                type="text"
-                placeholder="First Name"
-                className='infoInput'
-                name="firstname" />
-            <input
-                type="text"
-                placeholder="Last Name"
-                className='infoInput'
-                name="lastname" />
-        <input
-                type="text"
-                className='infoInput'
-                placeholder="username"
-                name="username" />
-            <span style={{fontSize: "12px"}}>Already have an account?</span>
-        <button className="signup__button">Sign Up</button>
-        </form>
-    );
+  return (
+<><a className="backBtn" href="/">Go Back</a>
+<form className="signup__container">
+      <div className="signupTitle">
+        <p>Want to connect with travelers like yourself?</p>
+        <h1>Sign Up to JetSettr</h1>
+        </div>
+
+      <input
+        type="text"
+        placeholder="First Name"
+        className="infoInput"
+        name="firstname"
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        className="infoInput"
+        name="lastname"
+      />
+      <input
+        type="text"
+        className="infoInput"
+        placeholder="Username"
+        name="username"
+      />
+
+      <input
+        type="text"
+        className="infoInput"
+        placeholder="Email Address"
+        name="email"
+      />
+
+      <input
+        type="text"
+        className="infoInput"
+        placeholder="Password"
+        name="password1"
+      />
+
+      <input
+        type="text"
+        className="infoInput"
+        placeholder="Confirm Password"
+        name="password2"
+      />
+
+      <span className="ahaa" style={{ fontSize: "22px" }}>Already have an account?</span>
+      <button className="signup__button">Sign Me Up</button>
+    </form></>
+  );
 }
 
-
 export default SignUp;
-
