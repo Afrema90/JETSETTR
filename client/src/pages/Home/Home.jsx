fc3cbda2f3e6f9c50efbe3ae43eabd4cac79a3be
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-03-10 14:24:59 -0500

    parsing error fix

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -5,85 +5,85 @@ const Home = () => {
   const [posts, setPosts] = useState([
     {
       id: 1,
-      username: “Cristiano”,
-      imageUrl: “https://images.unsplash.com/photo-1584907600887-9732fa3647ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80”,
-      caption: “Exploring the mountains”,
+      username: "Cristiano",
+      imageUrl: "https://images.unsplash.com/photo-1584907600887-9732fa3647ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80",
+      caption: "Exploring the mountains",
       likes: 56,
       comments: [
         {
           id: 1,
-          username: “KylieJenner”,
-          text: “Awesome picture!“,
+          username: "KylieJenner" ,
+          text: "Awesome picture!",
         },
         {
           id: 2,
-          username: “Cristiano”,
-          text: “Thanks!“,
+          username: "Cristiano",
+          text: "Thanks!",
         },
       ],
     },
     {
       id: 2,
-      username: “Beyonce”,
-      imageUrl: “https://images.unsplash.com/photo-1517035811173-b7659b8d0ddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80”,
-      caption: “Sunset at the beach”,
+      username: "Beyonce",
+      imageUrl: "https://images.unsplash.com/photo-1517035811173-b7659b8d0ddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+      caption: "Sunset at the beach",
       likes: 112,
       comments: [],
     },
     {
       id: 3,
-      username: “SelenaGomez”,
-      imageUrl: “https://images.unsplash.com/photo-1521747116042-5a810fda9664”,
-      caption: “The Golden Gate Bridge!!“,
+      username: "SelenaGomez",
+      imageUrl: "https://images.unsplash.com/photo-1521747116042-5a810fda9664",
+      caption: "The Golden Gate Bridge!!",
       likes: 654,
       comments: [],
     },
     {
       id: 4,
-      username: “MikeTyson”,
-      imageUrl: “https://images.unsplash.com/photo-1612278675615-7b093b07772d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80”,
-      caption: “Day 1 in Greece”,
+      username: "MikeTyson",
+      imageUrl: "https://images.unsplash.com/photo-1612278675615-7b093b07772d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80",
+      caption: "Day 1 in Greece",
       likes: 32,
       comments: [{
           id: 1,
-          username: “JakePaul”,
-          text: “I live there”,
+          username: "JakePaul",
+          text: "I live there",
         },
         {
           id: 2,
-          username: “MikeTyson”,
-          text: “Wow, really?!“,
+          username: "MikeTyson",
+          text: "Wow, really?!",
         },],
     },
     {
       id: 5,
-      username: “LionelMessi”,
-      imageUrl: “https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=960&q=80”,
-      caption: “time to Relax!“,
+      username: "LionelMessi",
+      imageUrl: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=960&q=80",
+      caption: "time to Relax!",
       likes: 765,
       comments: [],
     },
     {
       id: 6,
-      username: “KimKardashian”,
-      imageUrl: “https://images.unsplash.com/photo-1506197603052-3cc9c3a201bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80”,
-      caption: “What a sight!“,
+      username: "KimKardashian",
+      imageUrl: "https://images.unsplash.com/photo-1506197603052-3cc9c3a201bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+      caption: "What a sight!",
       likes: 543,
       comments: [],
     },
     {
       id: 7,
-      username: “ArianaGrande”,
-      imageUrl: “https://images.unsplash.com/photo-1517760444937-f6397edcbbcd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80”,
-      caption: “Should I take a dive?“,
+      username: "ArianaGrande",
+      imageUrl: "https://images.unsplash.com/photo-1517760444937-f6397edcbbcd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+      caption: "Should I take a dive?",
       likes: 23,
       comments: [],
     },
     {
       id: 8,
-      username: “JustinBieber”,
-      imageUrl: “https://images.unsplash.com/photo-1499363536502-87642509e31b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80”,
-      caption: “Ever seen a brighter night sky?“,
+      username: "JustinBieber",
+      imageUrl: "https://images.unsplash.com/photo-1499363536502-87642509e31b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+      caption: "Ever seen a brighter night sky?",
       likes: 124,
       comments: [],
     },
@@ -106,7 +106,7 @@ const UploadImage = () => {
     if (image && caption) {
       const newPost = {
         id: posts.length + 1,
-        username: “Raied”,
+        username: "Raied",
         imageUrl: URL.createObjectURL(image),
         caption,
         likes: 0,
@@ -114,34 +114,34 @@ const UploadImage = () => {
       };
       setPosts([newPost, ...posts]);
       setImage(null);
-      setCaption(“”);
+      setCaption("");
     }
   };
   return (
-    <div className=“upload-container”>
+    <div className="upload-container">
       <form onSubmit={handleSubmit}>
-        <label htmlFor=“image-upload” className=“upload-label”>
-          <i className=“fas fa-cloud-upload-alt”></i> +
+        <label htmlFor="image-upload" className="upload-label">
+          <i className="fas fa-cloud-upload-alt"></i> +
         </label>
         <input
-          type=“file”
-          id=“image-upload”
-          className=“file-input”
+          type="file"
+          id="image-upload"
+          className="file-input"
           onChange={handleImageUpload}
         />
         <input
-          type=“text”
-          placeholder=“Caption”
+          type="text"
+          placeholder="Caption"
           value={caption}
           onChange={handleCaptionChange}
         />
-        <button type=“submit”>Post</button>
+        <button type="submit">Post</button>
       </form>
       {image && (
         <img
           src={URL.createObjectURL(image)}
-          alt=“Uploaded Image”
-          className=“uploaded-image”
+          alt="Uploaded Image"
+          className="uploaded-image"
         />
       )}
     </div>
@@ -174,7 +174,7 @@ const UploadImage = () => {
             ...post.comments,
             {
               id: post.comments.length + 1,
-              username: “Raied”,
+              username: "Raied",
               text: newComment,
             },
           ],
@@ -183,33 +183,33 @@ const UploadImage = () => {
       return post;
     });
     setPosts(updatedPosts);
-    setNewComment(“”);
+    setNewComment("");
   };
   return (
-    <div className=“feed”>
+    <div className="feed">
       {posts.map((post) => (
-        <div key={post.id} className=“post”>
-          <div className=“post-header”>
-            <div className=“username”>{post.username}</div>
+        <div key={post.id} className="post">
+          <div className="post-header">
+            <div className="username">{post.username}</div>
           </div>
           <div onClick={() => handleLikeClick(post.id)}>
-            <img src={post.imageUrl} alt=“post” />
+            <img src={post.imageUrl} alt="post" />
           </div>
-          <div className=“post-footer”>
-            <div className=“likes”>{post.likes} likes</div>
-            <div className=“caption”>{post.caption}</div>
-            <div className=“comments”>
+          <div className="post-footer">
+            <div className="likes">{post.likes} likes</div>
+            <div className="caption">{post.caption}</div>
+            <div className="comments">
               {post.comments.map((comment) => (
-                <div key={comment.id} className=“comment”>
-                  <span className=“username”>{comment.username}</span>
-                  <span className=“text”>{comment.text}</span>
+                <div key={comment.id} className="comment">
+                  <span className="username">{comment.username}</span>
+                  <span className="text">{comment.text}</span>
                 </div>
               ))}
             </div>
-            <div className=“comment-form”>
+            <div className="comment-form">
               <input
-                type=“text”
-                placeholder=“Add a comment...”
+                type="text"
+                placeholder="Add a comment..."
                 value={newComment}
                 onChange={handleCommentChange}
               />
@@ -222,3 +222,5 @@ const UploadImage = () => {
     </div>
   );
   }
+
+  export default Home;
