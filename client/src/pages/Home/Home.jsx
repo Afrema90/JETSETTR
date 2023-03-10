25d5ec50593fda5a0f8829cb75e0d305b84f183f
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-03-10 13:23:39 -0500

    added heroku-postbuild script to package.json

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from ‘react’;
-import ‘./Home.css’;
+import React, { useState } from "react";
+ import "./Home.css";
 const Home = () => {
   const [posts, setPosts] = useState([
     {
