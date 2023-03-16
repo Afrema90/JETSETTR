1308dea7c512803afb499b6a11cfb2f2e12b612a
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-03-16 15:51:20 -0400

    trying different server stuff

--- server/server.js
+++ server/server.js
@@ -37,9 +37,9 @@ app.use(express.json());
 
 if (process.env.NODE_ENV) {
   //static folder add
-   app.use(express.static('app/client/build'));
+   app.use(express.static('client/build'));
    app.get("*", function (req, res) {
-   res.sendFile(path.resolve(__dirname , "app/client/build", "index.html"));
+   res.sendFile(path.resolve(__dirname , "client/build", "index.html"));
     });
   }
 
@@ -64,3 +64,4 @@ const startApolloServer = async (typeDefs, resolvers) => {
 // Call the async function to start the server
   startApolloServer(typeDefs, resolvers); 
 
+cd slient
