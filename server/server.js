dd1773eff2e2b1fcdf2669b9a5599c7e8e8e3c5e
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-03-16 16:14:06 -0400

    m

--- server/server.js
+++ server/server.js
@@ -20,16 +20,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-app.use(express.static(path.join(__dirname, '..', 'build')));
-app.use(express.static('public'));
+// app.use(express.static(path.join(__dirname, '..', 'build')));
+// app.use(express.static('public'));
 
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+// });
 
-app.get('/*', (req, res) => { 
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('/*', (req, res) => { 
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 
 if (process.env.NODE_ENV) {
